0cf04526ee261081125415c1fbd2cf6ba33bd842
cpetrescu96 <cristi.vl96@gmail.com>
2024-02-12 22:38:06 +0200

    feat: implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { finished } from 'stream/promises';
+
 const write = async () => {
-    // Write your code here 
+
+    const writeToFile = async () => {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+        const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+        const output = fs.createWriteStream(filePath);
+
+        output.write('Hello, ');
+        output.write('world!');
+        output.end();
+
+        process.stdin.pipe(output);
+
+        process.stdin.on('end', () => {
+            console.log('Finished writing to file');
+        });
+
+        output.on('error', err => {
+            console.error('An error occurred:', err);
+        });
+
+        await finished(output);
+        // console.log('File written successfully.');
+    }
+
+    try {
+        await writeToFile();
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await write();
