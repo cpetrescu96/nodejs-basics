8d1110a4aa14529480f1a8ef78fe4af0f8069431
cpetrescu96 <cristi.vl96@gmail.com>
2024-02-12 20:24:01 +0200

    feat: parse command line arguments and log in console

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const argsObj = {};
+    for (let i = 0; i < args.length; i += 2) {
+        argsObj[args[i].slice(2)] = args[i + 1];
+    }
+    console.log(Object.entries(argsObj).map(([key, value]) => `${key} is ${value}`).join(', '));
 };
 
 parseArgs();
