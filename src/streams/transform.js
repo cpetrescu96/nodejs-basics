d2a51e9d260667394ffd19fd30377afa96fc5065
cpetrescu96 <cristi.vl96@gmail.com>
2024-02-13 00:37:02 +0200

    feat: implement function that reads data from process.stdin, reverses text using Transform Stream and then writes it into process.stdout

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,37 @@
+import { Transform } from 'stream';
+import readline from 'readline';
+
 const transform = async () => {
-    // Write your code here 
+    
+    const reverseTransformFile = async () => {
+
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        const reverseTransform = new Transform({
+            transform(chunk, encoding, callback) {
+                process.stdout.write('Reversed text using Transform Stream: ');
+                this.push(chunk.toString().split('').reverse().join(''));
+                callback();
+            }
+        });
+        
+        rl.question('============= Please enter some text - ', (answer) => {
+            const string = String(answer).trim();
+            reverseTransform.write(answer);
+            rl.close();
+        });
+
+        reverseTransform.pipe(process.stdout);
+    };
+    
+    try {
+        await reverseTransformFile();
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await transform();
