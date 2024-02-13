6ae23567115f53964ce5e9f1d8c6e02f60bdcd17
cpetrescu96 <cristi.vl96@gmail.com>
2024-02-13 08:40:33 +0200

    feat: implement IPC connection between main master process and child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,30 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const scriptPath = path.join(__dirname, 'files', 'script.js');
+    const child = spawn('node', [scriptPath, ...args], { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    child.stdout.on('data', (message) => {
+        console.log(`Master has Received the following message from children: \n ` + message.toString());
+        // child.stdout.pipe(process.stdout);
+    });
+
+    child.stderr.on('data', (error) => {
+        console.error(`An error occurred: ${error} \n`);
+    });
+
+    child.on('exit', (code, signal) => {
+        console.log(`Child process exited with code ${code} and signal ${signal} \n`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['--name', 'John Doe', '--anotherOne'] );
