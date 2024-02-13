9a35057580682887eeeb8a177b6349b4625c3339
cpetrescu96 <cristi.vl96@gmail.com>
2024-02-13 07:22:55 +0200

    feat: main worker thread implement function that creates number of worker threads (equal to the number of host machine logical CPU cores) from file worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const performCalculations = async () => {
-    // Write your code here
+    
+    const cpus = os.cpus().length;
+    const workers = [];
+    const results = [];
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const workerFilePath = path.join(__dirname, 'worker.js');
+
+    for (let i = 0; i < cpus; i++) {
+        workers[i] = new Worker(workerFilePath);
+    }
+
+    for (let i = 0; i < cpus; i++) {
+        workers[i].on('message', (result) => {
+            results.push({ status: 'resolved', data: result});
+        });
+        workers[i].on('error', () => {
+            results.push({ status: 'error', data: null });
+        });
+        workers[i].on('exit', () => {
+            if (results.length === cpus) {
+                console.log(results);
+            }
+        });
+    }
+
+    for (let i = 0; i < cpus; i++) {
+        workers[i].postMessage(i + 10);
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,21 @@
+import { parentPort } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    parentPort.on('message', (n) => {
+        const result = nthFibonacci(n);
+        parentPort.postMessage(result);
+        process.exit(0);
+    });
+
+    // close the worker thread
+    parentPort.on('close', () => {
+        process.exit(0);
+    });
+    
 };
 
 sendResult();
